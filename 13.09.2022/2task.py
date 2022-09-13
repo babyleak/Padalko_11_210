eb7f4828f4a69f034bf3cf4dfb6b3d74bf3969f3
Daniil Padalko <dan4ykplay@gmail.com>
2022-09-13 18:19:20 +0300

    2task.py has been added

---
+++ 13.09.2022/2task.py
@@ -0,0 +1,12 @@
+start = 1
+end = -1
+
+def IsPointInSquare(x, y):
+    return (start >= x >= end) and (end <= y <= start)
+
+x = float(input())
+y = float(input())
+if IsPointInSquare(x, y):
+    print('yes')
+else:
+    print('no')
