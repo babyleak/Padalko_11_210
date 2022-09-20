a4319330bc52736d3ea9890a396f6f2ffaa6ef5c
Daniil Padalko <dan4ykplay@gmail.com>
2022-09-20 17:08:56 +0300

    new change has been added in 1task.py

--- 20.09.2022/1task.py
+++ 20.09.2022/1task.py
@@ -1,9 +1,13 @@
-def print_rectangle(m, n):
-    for i in range(m):
-        for j in range(n):
-            print('1', end='')
-        print()
+x = int(input(""))
+n = int(input(""))
 
-m = int(input())
-n = int(input())
-print_rectangle(m, n)
+for i in range(n):
+    if i == 0 or i == n - 1:
+        for y in range(x):
+            print('1', end=' ')
+    else:
+        print('1', end=' ')
+        for y in range(1, x - 1):
+            print(' ', end=' ')
+        print('1', end=' ')
+    print()
