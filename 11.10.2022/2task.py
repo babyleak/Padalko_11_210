9fd66dea2e99b1a60cbcd536fe4f2d7a40feab61
Daniil Padalko <dan4ykplay@gmail.com>
2022-10-18 11:46:07 +0300

    new change in 2task

--- 11.10.2022/2task.py
+++ 11.10.2022/2task.py
@@ -1,8 +1,34 @@
-from random import randint
+import random
 
-students = list(map(int(input().split())))
-group_list = []
-for group in range(1, 3):
-    number_students = randint(*students)
+names = ["Мансур", "Алим", "Изида", "Георгий", "Даниил", "Данила", "Элина", "Диана", "Ильназ", "Фарида", "Роман", "Юлия",
+         "Тихон", "Данила", "Рамис"]
 
-print(max(group_list, key=lambda x: [1])[0])
+students = 20
+groups = 3
+
+students_list =[]
+
+for x in range(groups):
+    this_group = []
+    for i in range(students):
+        this_name = random.choice(names)
+        this_group.append(this_name)
+    students_list.append(this_group)
+    print(this_group)
+
+set_names = []
+set_groups = []
+print("set:")
+for this_group in students_list:
+    this_set = set(this_group)
+    set_groups.append(this_set)
+    print(this_set)
+
+print("setnames")
+
+set_names = set_groups[0].intersection(set_groups[1], set_groups[2])
+print(set_names)
+
+print("unique")
+unique_names = set_groups[0].symmetric_difference(set_groups[1]).symmetric_difference(set_groups[2])
+print(unique_names)
